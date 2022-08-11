2756d9b86eb7c19769250b4797d44d154ab149de
serge <xeliffer@gmail.com>
2022-08-11 14:12:05 +0300

    fix middleware

--- wordpress_auth/middleware.py
+++ wordpress_auth/middleware.py
@@ -3,6 +3,10 @@ from django.utils.functional import SimpleLazyObject
 from .utils import get_wordpress_user
 
 
-class WordPressAuthMiddleware(object):
-    def process_request(self, request):
+class WordPressAuthMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
         request.wordpress_user = SimpleLazyObject(lambda: get_wordpress_user(request))
+        return self.get_response(request)
